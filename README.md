ae1ddbbb5fb991804e958516c1cea1fafd5ad1c3
Emmanuel Raviart <emmanuel@raviart.com>
2021-02-08 14:28:51 +0100

    Filter extracted amendments for PLFSS 2020-2021.

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Diverses études des amendements et projets de lois de l'Assemblée, pour le pro
 ### Extraction des textes des amendements sous forme de fichier CSV
 
 ```bash
-npx babel-node --extensions ".ts" --max-old-space-size=4096 src/scripts/extraire_textes_amendements.ts ../../tricoteuses/assemblee-data/ > amendements.csv
+npx babel-node --extensions ".ts" --max-old-space-size=4096 src/scripts/extraire_textes_amendements_nouveaux_articles_plfss_2020-2021.ts ../../tricoteuses/assemblee-data/ > textes_amendements_nouveaux_articles_plfss_2020-2021.csv
 ```
---
+++ src/scripts/extraire_textes_amendements_nouveaux_articles_plfss_2020-2021.ts
@@ -0,0 +1,138 @@
+import {
+  ActeLegislatif,
+  AvantAApres,
+  DossierParlementaire,
+  Legislature,
+  TypeActeLegislatif,
+} from "@tricoteuses/assemblee"
+import {
+  EnabledDatasets,
+  loadAssembleeData,
+} from "@tricoteuses/assemblee/lib/loaders"
+import assert from "assert"
+import commandLineArgs from "command-line-args"
+// import fs from "fs-extra"
+import Papa from "papaparse"
+
+const optionsDefinitions = [
+  {
+    defaultOption: true,
+    help: "directory containing Assemblée open data files",
+    name: "dataDir",
+    type: String,
+  },
+]
+const options = commandLineArgs(optionsDefinitions)
+
+function* iterActes(
+  actes: ActeLegislatif[],
+): Generator<ActeLegislatif, void, void> {
+  for (const acte of actes) {
+    yield acte
+    if (acte.actesLegislatifs !== undefined) {
+      for (const childActe of iterActes(acte.actesLegislatifs)) {
+        yield childActe
+      }
+    }
+  }
+}
+
+function* iterDocumentsLegislatifsUid(
+  dossier: DossierParlementaire,
+): Generator<string, void, void> {
+  for (const acte of iterActes(dossier.actesLegislatifs)) {
+    if (acte.texteAdopteRef !== undefined) {
+      yield acte.texteAdopteRef
+    }
+    if (acte.texteAssocieRef !== undefined) {
+      if (
+        [
+          TypeActeLegislatif.DepotInitiativeNavetteType,
+          TypeActeLegislatif.DepotInitiativeType,
+        ].includes(acte.xsiType)
+      ) {
+        yield acte.texteAssocieRef
+      } else if (
+        ![
+          TypeActeLegislatif.EtudeImpactType,
+          TypeActeLegislatif.DepotRapportType,
+        ].includes(acte.xsiType)
+      ) {
+        if (!options.silent) {
+          console.warn(
+            `Dans le dossier législatif ${dossier.uid}, le texte associé ${acte.texteAssocieRef} est ignoré, car l'acte est du type "${acte.xsiType}", qui n'est pas supporté.`,
+          )
+        }
+      }
+    }
+  }
+}
+
+async function main() {
+  const { dossierParlementaireByUid } = loadAssembleeData(
+    options.dataDir,
+    EnabledDatasets.DossiersLegislatifs,
+    Legislature.Quinze,
+  )
+  const documentsLegislatifsUid = new Set<string>()
+  for (const dossierUid of [
+    "DLR5L15N37831", // PLFSS 2020
+    "DLR5L15N40633", // PLFSS 2021
+  ]) {
+    const dossier = dossierParlementaireByUid[dossierUid]
+    assert.notStrictEqual(dossier, undefined)
+    for (const documentLegislatifUid of iterDocumentsLegislatifsUid(dossier!)) {
+      documentsLegislatifsUid.add(documentLegislatifUid)
+    }
+  }
+
+  const { amendementByUid } = loadAssembleeData(
+    options.dataDir,
+    EnabledDatasets.Amendements,
+    Legislature.Quinze,
+  )
+  const amendementsExport: {
+    dispositif?: string
+    exposeSommaire?: string
+  }[] = []
+  for (const amendement of Object.values(amendementByUid)) {
+    const {
+      corps,
+      cycleDeVie,
+      pointeurFragmentTexte,
+      texteLegislatifRef,
+    } = amendement
+    if (!documentsLegislatifsUid.has(texteLegislatifRef)) {
+      continue
+    }
+    if (
+      ![AvantAApres.Avant, AvantAApres.Apres].includes(
+        pointeurFragmentTexte.division.avantAApres,
+      )
+    ) {
+      continue
+    }
+    if (cycleDeVie.etatDesTraitements.etat.libelle !== "Discuté") {
+      continue
+    }
+    const { contenuAuteur } = corps
+    if (contenuAuteur === undefined) {
+      continue
+    }
+    amendementsExport.push({
+      dispositif: contenuAuteur.dispositif,
+      exposeSommaire: contenuAuteur.exposeSommaire,
+    })
+  }
+  const amendementsCsv = Papa.unparse(amendementsExport)
+  console.log(amendementsCsv)
+}
+
+main()
+  .then(() => {
+    process.exit(0)
+  })
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
