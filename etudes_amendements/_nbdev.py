edf0ff6fc1496d4a9c32a81dc6e2f8fc70be66a6
Benoit Courty <benoit.courty@assemblee-nationale.fr>
2021-02-26 09:47:41 +0100

    Amendements par année

--- etudes_amendements/_nbdev.py
+++ etudes_amendements/_nbdev.py
@@ -2,7 +2,7 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"load_data": "02_word_count.ipynb",
+index = {"load_data": "eda_ref_amendements.ipynb",
          "amdt": "02_word_count.ipynb",
          "build_word_cloud": "02_word_count.ipynb",
          "display_distinct": "eda_ref_amendements.ipynb"}
