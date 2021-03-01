417d27a2d09c7a364a8f1cf880de6b7f94e279fd
Benoit Courty <benoit.courty@assemblee-nationale.fr>
2021-03-01 09:31:47 +0100

    Etude nombre de soutiens

--- etudes_amendements/_nbdev.py
+++ etudes_amendements/_nbdev.py
@@ -5,6 +5,7 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 index = {"load_data": "eda_ref_amendements.ipynb",
          "amdt": "02_word_count.ipynb",
          "build_word_cloud": "02_word_count.ipynb",
+         "plot_serie": "eda_ref_amendements.ipynb",
          "display_distinct": "eda_ref_amendements.ipynb"}
 
 modules = ["word_count.py",
